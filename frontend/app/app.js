d6679839b3b2cfe144df78ad2e12bab81dba070f
beastsanchez <beastsanchez7@gmail.com>
2017-11-05 06:38:29 +0530

    Favicon removed from app.js Footer Ui changes Inject Comments removed from index

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -24,7 +24,7 @@ import LanguageProvider from 'containers/LanguageProvider';
 
 // Load the favicon, the manifest.json file and the .htaccess file
 /* eslint-disable import/no-unresolved, import/extensions */
-import '!file-loader?name=[name].[ext]!./images/favicon.ico';
+// import '!file-loader?name=[name].[ext]!./images/favicon.ico';
 
 import '!file-loader?name=[name].[ext]!./manifest.json';
 import 'file-loader?name=[name].[ext]!./.htaccess';
