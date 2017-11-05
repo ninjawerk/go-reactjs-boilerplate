155c285a5cb66964c49ee210e83cc565289e2b2d
beastsanchez <beastsanchez7@gmail.com>
2017-11-05 06:34:20 +0530

    Sample Secure Page

---
+++ frontend/app/containers/SecurePage/index.js
@@ -0,0 +1,27 @@
+/*
+ * HomePage
+ *
+ * This is the first thing users see of our App, at the '/' route
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
+ */
+
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+import messages from './messages';
+import {Button} from 'reactstrap';
+
+export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className="row h-100 justify-content-center align-items-center bg-light">
+        <div>
+          <img src="/public/img/goreact.png"/>
+        </div>
+      </div>
+    );
+  }
+}
